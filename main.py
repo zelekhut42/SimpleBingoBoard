7d73183c429d13edf2c488ad6bc87ae86d50bbed
Robert Curtis <bowninja42@gmail.com>
2024-06-29 18:17:44 -0400

    fixed spelling errors

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#   This program is a simple bingo board origionaly created for personal use.
-#   It reads a specialy created CSV file in order to get the text for the bino board squares.
-#   If a bingo(all of one row, column, or diagonal is fully filled) is reached this program will print out that a new bingo has occured.
+#   This program is a simple bingo board origionally created for personal use.
+#   It reads a specially created CSV file in order to get the text for the bino board squares.
+#   If a bingo(all of one row, column, or diagonal is fully filled) is reached this program will print out that a new bingo has occurred.
 #   This behavior can be changed in bingo_board.py.
-#   The specialy created CSV must contain a column titled "regular space", a column titled "free space", and my contain additional columns.
+#   The specially created CSV must contain a column titled "regular space", a column titled "free space", and my contain additional columns.
 #   The regular space column contains the text for the squares for the bingo board, and at a minimum must contain enough entries to populate all the squares of the bingo board or you will get an error.
 #   The free space column contains the text for the free space square.
-#   Any additional columns need a title that is the exact same(case sensitive) as an entry in the recular space column, and when that entry is selected to be in the bingo board it is replaced with a random entry from this column
+#   Any additional columns need a title that is the exact same(case sensitive) as an entry in the regular space column, and when that entry is selected to be in the bingo board it is replaced with a random entry from this column
 
 #imports
 import data_processing
